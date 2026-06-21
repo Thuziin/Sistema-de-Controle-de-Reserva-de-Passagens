ef758f235dbd35fad1448e22ad73c276f34ee6ab
Arthur dos Santos <dss.arthur0@gmail.com>
2026-06-21 19:25:25 -0300

    feat: adicionando component do formulário de registro de passageiro

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import CardTicket from "../components/CardTicket";
+import ModalPassenger from "../components/ModalPassenger";
 import ModalSeat from "../components/ModalSeat";
 import logo from "../assets/Logo3.png";
 import type { Ticket } from "../types/Ticket";
@@ -8,10 +9,15 @@ import type { Ticket } from "../types/Ticket";
 import "../styles/Home.css";
 
 function Home() {
-  const [modalAberto, setModalAberto] = useState(false);
+  const [modalSeatAberto, setModalSeatAberto] = useState(false);
+  const [modalPassengerAberto, setModalPassengerAberto] = useState(false);
+
   const [tickets, setTickets] = useState<Ticket[]>([]);
   const [ticketSelecionado, setTicketSelecionado] = useState<Ticket | null>(null);
   const [pesquisa, setPesquisa] = useState("");
+  const [assentoSelecionado, setAssentoSelecionado] = useState<string | null>(null);
+
+  
 
   useEffect(() => {
     fetch("http://localhost:3000/api/passagens")
@@ -23,21 +29,14 @@ function Home() {
   }, []);
 
   const ticketsFiltrados = tickets.filter((ticket) =>
-    ticket.destino
-      .toLowerCase()
-      .includes(pesquisa.toLowerCase())
+    ticket.destino.toLowerCase().includes(pesquisa.toLowerCase()),
   );
 
   return (
     <>
-      <Header
-        titulo="Início"
-        logo={logo}
-        mostrarLinkMyTicket
-      />
+      <Header titulo="Início" logo={logo} mostrarLinkMyTicket />
 
       <main className="tickets-container">
-
         <div className="search-container">
           <input
             type="text"
@@ -45,13 +44,10 @@ function Home() {
             className="search-input"
             value={pesquisa}
             onChange={(e) => setPesquisa(e.target.value)}
-
           />
         </div>
 
-        <h2 className="section-title">
-          Sugestões
-        </h2>
+        <h2 className="section-title">Sugestões</h2>
 
         <div className="cards-grid">
           {ticketsFiltrados.map((ticket) => (
@@ -60,18 +56,33 @@ function Home() {
               ticket={ticket}
               onClick={() => {
                 setTicketSelecionado(ticket);
-                setModalAberto(true);
+                setModalSeatAberto(true);
               }}
             />
           ))}
         </div>
-
       </main>
 
       <ModalSeat
-        aberto={modalAberto}
-        fechar={() => setModalAberto(false)}
+        aberto={modalSeatAberto}
+        fechar={() => setModalSeatAberto(false)}
         ticket={ticketSelecionado}
+        avancarModalPassenger={(assento) => {
+          setAssentoSelecionado(assento);
+          setModalSeatAberto(false);
+          setModalPassengerAberto(true);
+        }}
+      />
+
+      <ModalPassenger
+        aberto={modalPassengerAberto}
+        fechar={() => setModalPassengerAberto(false)}
+        ticket={ticketSelecionado}
+        assentoSelecionado={assentoSelecionado}
+        retonarModalSeat={() => {
+          setModalPassengerAberto(false);
+          setModalSeatAberto(true);
+        }}
       />
     </>
   );
