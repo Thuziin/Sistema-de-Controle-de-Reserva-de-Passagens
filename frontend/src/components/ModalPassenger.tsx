8cca3f4f1a32473f4b4bca628415b6502eef89e2
Arthur dos Santos <dss.arthur0@gmail.com>
2026-06-21 19:23:53 -0300

    feat: conexão com backend e registro do passageiro de uma passagem

--- frontend/src/components/ModalPassenger.tsx
+++ frontend/src/components/ModalPassenger.tsx
@@ -1,26 +1,104 @@
 import { useState } from "react";
 import type { Ticket } from "../types/Ticket";
-// import "../styles/ModalPassenger.css";
+import "../styles/ModalPassenger.css";
 
 interface ModalPassengerProps {
-
+    aberto: boolean;
+    fechar: () => void;
+    ticket: Ticket | null;
+    assentoSelecionado: string | null;
+    retonarModalSeat: () => void;
 }
 
-function ModalPassenger({}: ModalPassengerProps) {
+function ModalPassenger({ aberto, fechar, ticket, assentoSelecionado, retonarModalSeat } : ModalPassengerProps) {
+    const [p_nome, setPNome] =  useState("");
+    const [u_nome, setUNome] = useState("");
+    const [cpf_passageiro, setCPF] = useState("");
+
+
+    if (!aberto) return null;
+    
+    async function registrarPassageiro() {
+        if (!assentoSelecionado || !p_nome || !u_nome || !cpf_passageiro || !ticket) {
+            alert ("Os dados do formulário são obrigatórios");
+            return ;
+        }
+
+        const cpf = localStorage.getItem("cpf");
+
+        try {
+            const response = await fetch("http://localhost:3000/api/comprar", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    cpf,
+                    passagemId: ticket.id,
+                    assento: assentoSelecionado
+                }),
+            });
+
+            if (!response.ok) {
+                throw new Error();
+            }
+
+            alert("Passagem adquirida!");
+
+            fechar();
+
+            window.location.reload();
+        } catch (err) {
+            alert(err);
+        }
+    } 
+
     return (
-        <div className="overlay">
-            <div className="modal">
-                <h2>Cadastro de Passageiros</h2>
+        <div className="overlayP">
+            <div className="modalP">
+                <h2 className="tituloP">Cadastro de Passageiros</h2>
                 <div className="formulario">
-                    <input type="text" id="p_nome" placeholder="Primeiro nome" required/>
-                    <input type="text" id="u_nome" placeholder="Sobrenome" required/>
-                    <input type="text" id="cpf_passageiro" placeholder="CPF" required maxLength={11}/>
+
+                    <div className="input-groupP">
+                        <input 
+                            type="text" 
+                            placeholder="Primeiro nome"
+                            value={p_nome}
+                            onChange={(e) => setPNome(e.target.value)} 
+                            required/>
+                    </div>
+
+                    <div className="input-groupP">
+                        <input 
+                            type="text"
+                            placeholder="Sobrenome" 
+                            value={u_nome} 
+                            onChange={(e) => setUNome(e.target.value)}
+                            required/>
+
+                    </div>
+
+                    <div className="input-groupP">
+                        <input 
+                            type="tel" 
+                            placeholder="CPF" 
+                            value={cpf_passageiro}
+                            onChange={(e) => setCPF(e.target.value)}
+                            required 
+                            maxLength={11}/>
+                    </div>
+
+
+                    <h3 className="tituloP">
+                        Assento Selcionado: {assentoSelecionado}
+                    </h3>
                 </div>
 
-                <button className="btn">Voltar</button>
-                <button className="btn">Cadastrar</button>
+                <button className="btnP" onClick={retonarModalSeat}>Voltar</button>
+                <button className="btnP" onClick={registrarPassageiro}>Cadastrar</button>
             </div>
         </div>
     )
 }
 
+export default ModalPassenger;
