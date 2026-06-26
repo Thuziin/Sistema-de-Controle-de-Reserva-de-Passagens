b9e02461cf15c813cc4bb1dfacdd55766a320f3a
Arthur dos Santos <dss.arthur0@gmail.com>
2026-06-25 22:43:48 -0300

    fix: mudando query adicionando inner/left join

--- backend/src/routes/historicoRoutes.ts
+++ backend/src/routes/historicoRoutes.ts
@@ -9,19 +9,22 @@ router.get("/historico/:cpf", async (req, res) => {
         const resultado = await pool.query(
             `
             SELECT
-              v.id,
-              partida.cidade AS origem,
-              chegada.cidade AS destino,
-            TO_CHAR(p.data, 'DD/MM/YYYY') AS data,
-            TO_CHAR(p.hora, 'HH24:MI') AS hora,
-              partida.identificador AS aeroporto
+                v.id,
+                partida.cidade AS origem,
+                chegada.cidade AS destino,
+                TO_CHAR(p.data,'DD/MM/YYYY') AS data,
+                TO_CHAR(p.hora,'HH24:MI') AS hora,
+                partida.identificador AS aeroporto,
+                p.assento,
+                p.pnome_passageiro || ' ' || p.unome_passageiro AS passageiro
             FROM passagem p
-            JOIN voo v ON p.voo_id = v.id
-            JOIN aeroporto partida
-            ON v.aeroporto_id_partida = partida.identificador
-            JOIN aeroporto chegada
-            ON v.aeroporto_id_chegada = chegada.identificador
-            WHERE p.pessoa_cpf_compra = $1
+            INNER JOIN voo v
+                ON p.voo_id = v.id
+            LEFT JOIN aeroporto partida
+                ON v.aeroporto_id_partida = partida.identificador
+            LEFT JOIN aeroporto chegada
+                ON v.aeroporto_id_chegada = chegada.identificador
+            WHERE p.pessoa_cpf_compra = $1;
             `,
             [cpf]
         );
