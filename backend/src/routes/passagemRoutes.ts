f92d16092c04c471eaa21d81744d01f8adc13a10
Arthur dos Santos <dss.arthur0@gmail.com>
2026-06-25 22:41:55 -0300

    fix: adicionando verificação quanto a data e hora da passagem

--- backend/src/routes/passagemRoutes.ts
+++ backend/src/routes/passagemRoutes.ts
@@ -5,29 +5,24 @@ const router = Router();
 
 router.get("/passagens", async (req, res) => {
     try {
-
         const resultado = await pool.query(`
-      SELECT
-        p.id,
-        partida.cidade AS origem,
-        chegada.cidade AS destino,
-        TO_CHAR(p.data, 'DD/MM/YYYY') AS data,
-        TO_CHAR(p.hora, 'HH24:MI') AS hora,
-        partida.identificador AS aeroporto
-
-      FROM passagem p
-
-      JOIN voo v
-        ON p.voo_id = v.id
-
-      JOIN aeroporto partida
-        ON v.aeroporto_id_partida = partida.identificador
-
-      JOIN aeroporto chegada
-        ON v.aeroporto_id_chegada = chegada.identificador
-
-      WHERE p.pessoa_cpf_compra = '00000000000'
-    `);
+            SELECT
+                p.id,
+                partida.cidade AS origem,
+                chegada.cidade AS destino,
+                TO_CHAR(p.data, 'DD/MM/YYYY') AS data,
+                TO_CHAR(p.hora, 'HH24:MI') AS hora,
+                partida.identificador AS aeroporto
+            FROM passagem p
+            JOIN voo v 
+                ON p.voo_id = v.id
+            JOIN aeroporto partida 
+                ON v.aeroporto_id_partida = partida.identificador
+            JOIN aeroporto chegada 
+                ON v.aeroporto_id_chegada = chegada.identificador
+            WHERE p.pessoa_cpf_compra = '00000000000'
+              AND (p.data + p.hora) > CURRENT_TIMESTAMP
+        `);
 
         res.json(resultado.rows);
 
